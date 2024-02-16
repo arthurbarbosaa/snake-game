143edac7db59179b2371e9eb7ef7b549cdcbb855
Arthur Barbosa <arthur.com50@gmail.com>
2024-02-16 19:31:31 -0300

    add function game over.

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const canvas = document.querySelector("canvas") // link do canvas do html com o js
 const context = canvas.getContext("2d") // link do contexto do canvas
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
 
 const audio = new Audio("assets/audio.mp3")
+const initialPosition = {x: 300, y: 300}
 const size = 30
 // é importante variar as posicoes der 30 em 30 pois a cobra tem o tamanho 30
 
-const snake = [
-    {x: 300, y: 300},
-    {x: 330, y: 300},
-]
+const incrementScore = () => {
+    score.innerText = +score.innerText + 10
+}
+
+let snake = [initialPosition]
 
 const randomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
@@ -33,11 +39,11 @@ const drawFood = () => {
 }
 
 const drawSnake = () => {
-    context.fillStyle = "#ddd"
+    context.fillStyle = "green"
 
     snake.forEach((position, index) => { //percorrer o array snake por cada elemenmto
         if(index == snake.length - 1) {
-            context.fillStyle = "white"
+            context.fillStyle = "lightGreen"
         }
 
         context.fillRect(position.x, position.y, size, size)
@@ -89,6 +95,7 @@ const checkEat = () => {
     const head = snake[snake.length - 1]
 
     if(head.x == food.x && head.y == food.y) {
+        incrementScore()
         snake.push(head)
         audio.play()
 
@@ -109,12 +116,25 @@ const checkEat = () => {
 const checkColision = () => {
     const head = snake[snake.length - 1]
     const canvasLimit = canvas.width - size
+    const neckIndex = snake.length - 2 // pegar a primeira posicao que nao é a cabeca
 
     const wallColision = head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
 
-    if(wallColision){
-        alert("perdeu")
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y 
+    })
+
+    if(wallColision || selfCollision){
+        gameOver()
     }
+}
+
+const gameOver = () => {
+    direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText = score.innerText
+    canvas.style.filter = "blur(2px)"
 }
 
 const gameLoop = () => {
@@ -151,4 +171,15 @@ document.addEventListener("keydown", ({ key }) => {
     if(key == "ArrowUp" && direction != "down") {
         direction = "up"
     }
+})
+
+buttonPlay.addEventListener("click", () => {
+    score.innerText = "00"
+    menu.style.display = "none"
+    canvas.style.filter = "none"
+
+    snake = [initialPosition]
+
+    food.x = randomPosition()
+    food.y = randomPosition()
 })
